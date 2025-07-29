88d8120674acec5ebb3d236d4acfd9baabed030a
idubusomuch <idubusomuch@gmail.com>
2025-07-29 16:00:46 +0900

    feat: jsx 방식 적용을 위해 확장자 변경 (#14)

--- src/constants/category/menuItems.tsx
+++ src/constants/category/menuItems.tsx
@@ -1,5 +1,12 @@
 export const CATEGORY_MENU_ITEMS = [
-  { label: '보드Pick!', href: '/today' },
+  {
+    label: (
+      <>
+        보드<span className="text-primary-500">Pick!</span>
+      </>
+    ),
+    href: '/today',
+  },
   { label: '보드큐 추천', href: '/recommend' },
   { label: '보드게임 찾기', href: '/search' },
 ];
