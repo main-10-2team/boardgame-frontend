c1f5cb5eb19c4c7049d75104a5a711daeba2741d
jngmnj <exorbitance@naver.com>
2025-08-01 17:22:38 +0900

    chore: Header 메뉴 스타일, href 수정 (#24)

--- src/constants/category/menuItems.tsx
+++ src/constants/category/menuItems.tsx
@@ -2,11 +2,11 @@ export const CATEGORY_MENU_ITEMS = [
   {
     label: (
       <>
-        보드<span className="text-primary-500 font-bold">Pick!</span>
+        보드<span className="text-primary-500 ml-0.5 font-bold">Pick!</span>
       </>
     ),
     href: '/today',
   },
   { label: '보드큐 추천', href: '/recommend' },
-  { label: '보드게임 찾기', href: '/search' },
+  { label: '보드게임 찾기', href: '/games' },
 ];
