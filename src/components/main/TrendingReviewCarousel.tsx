411fd81f6edd300488fc102318fd75451eaa4af3
jngmnj <exorbitance@naver.com>
2025-08-17 02:42:42 +0900

    chore: 메인페이지 리뷰섹션 수정 (#74)
    - overflow-hidden을 Swiper만 감싸서 버튼 나오게 수정

--- src/components/main/TrendingReviewCarousel.tsx
+++ src/components/main/TrendingReviewCarousel.tsx
@@ -17,40 +17,42 @@ export default function TrendingReviewCarousel({
   handleReviewClick,
 }: TrendingReviewCarouselProps) {
   return (
-    <div className="relative h-50 overflow-hidden" id="review">
-      <Swiper
-        slidesPerView={1}
-        direction={'vertical'}
-        spaceBetween={24}
-        loop={true}
-        autoplay={{ delay: 3000, disableOnInteraction: false }} // 자동 슬라이드 설정
-        modules={[Navigation, Autoplay]}
-        navigation={{
-          nextEl: '#review .theme-button-next',
-          prevEl: '#review .theme-button-prev',
-        }}
-        breakpoints={{
-          768: {
-            slidesPerView: 3,
-            direction: 'horizontal',
-          },
-        }}
-        className="h-full"
-      >
-        {reviews.map((review) => (
-          <SwiperSlide
-            key={review.review_id}
-            onClick={() => handleReviewClick(review)}
-          >
-            <ReviewItem
-              review={review}
-              isDisplayTitle={true}
-              isDisplayImage={true}
-              isDisplayDate={false}
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+    <div className="relative h-50" id="review">
+      <div className="overflow-hidden">
+        <Swiper
+          slidesPerView={1}
+          direction={'vertical'}
+          spaceBetween={24}
+          loop={true}
+          autoplay={{ delay: 3000, disableOnInteraction: false }} // 자동 슬라이드 설정
+          modules={[Navigation, Autoplay]}
+          navigation={{
+            nextEl: '#review .theme-button-next',
+            prevEl: '#review .theme-button-prev',
+          }}
+          breakpoints={{
+            768: {
+              slidesPerView: 3,
+              direction: 'horizontal',
+            },
+          }}
+          className="h-full"
+        >
+          {reviews.map((review) => (
+            <SwiperSlide
+              key={review.review_id}
+              onClick={() => handleReviewClick(review)}
+            >
+              <ReviewItem
+                review={review}
+                isDisplayTitle={true}
+                isDisplayImage={true}
+                isDisplayDate={false}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
       <div className="pointer-events-none absolute top-1/2 left-1/2 z-20 hidden w-full -translate-x-1/2 -translate-y-1/2 transform justify-between px-4 md:flex lg:w-[calc(100%+120px)] lg:px-0">
         <button className="theme-button-prev pointer-events-auto">
           <RiArrowLeftLine className="text-2xl" />
